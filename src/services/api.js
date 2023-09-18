554b6577f89ed78cdecfcdfa1bea13e30dc25a36
iamtheluiz <iamtheluiz.dev@gmail.com>
2023-09-17 23:10:48 -0300

    refactor: set service endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.100:3000',
+  // baseURL: 'http://192.168.0.100:3000',
   // baseURL: "https://cinemais-api.luizvasconcellos.com.br"
+  baseURL: "https://cinemais-api.onrender.com"
 });
 
 export default api;
